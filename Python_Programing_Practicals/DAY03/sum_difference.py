1d7e98b78b36b391b847e993e7171b492f1cd430
MishmaKhan <167611744+MishmaKhan@users.noreply.github.com>
2024-04-21 20:56:34 +0500

    Add files via upload

---
+++ Python_Programing_Practicals/DAY03/sum_difference.py
@@ -0,0 +1,11 @@
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate sum and difference
+sum_result = num1 + num2
+difference_result = num1 - num2
+
+# Display the results
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
